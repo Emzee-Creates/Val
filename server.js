0702a56d1f967875294622e7f0d6f209e1e6c9bc
Emzee-Creates <106532087+Emzee-Creates@users.noreply.github.com>
2025-02-13 14:00:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -25,26 +25,42 @@ const transporter = nodemailer.createTransport({
 
 // Route to send email
 app.post('/send-email', (req, res) => {
+  // Get the file path for the attachment
+  const filePath = path.join(__dirname, 'attachments', 'Open Me!.zip');
+  
   const mailOptions = {
     from: process.env.MAIL_USER,
     to: 'emmanuelemoruwa356@gmail.com', // Replace with recipient's email
     subject: 'Happy Valentine\'s Day!',
-    text: 'Sending you lots of love on this special day Eriife! 💖',
+    text: `I would have loved to buy you thousands of flowers and a million boxes of chocolates and take you to the fanciest of restaurants for dinner on this special day, unfortunately, I can't. What I CAN do is remind you of how special you are to me.
+
+The attachment below has 3 files in it, divided into: I, Love and You.
+I - It's a personal message I wrote for you
+Love - They say music is the language of love and I tried to show that by making a playlist with the songs that feel most special to me.
+You - A short video I put together hopefully showing how I see You as a person. (P.S I'm not a very good video editor 😭😭).
+
+Happy Valentine's day my love. I love you more than words can describe. Keep being special to me.
+Sending you lots of Love on this special day Eriife! 💖
+Korede.`,
+    attachments: [
+      {
+        filename: 'Open Me!.zip',
+        path: filePath,
+      }
+    ]
   };
 
-   transporter.sendMail(mailOptions, (error, info) => {
-    // This will run regardless of success or failure:
-    return res.status().send('Check your Mail!');
-
+  transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.log('Error sending email:', error);  // Log the error if any
-      return res.status(500).send('Check your Mail!');
+      return res.status(500).send('Error sending email');
     } else {
-      console.log('Check Your Mail:');  // Log the success if email sent
+      console.log('Email sent successfully:', info.response);  // Log the success if email sent
       return res.status(200).send('Email sent successfully!');
     }
   });
 });
+
 // Start the server
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on: ${PORT}`);
